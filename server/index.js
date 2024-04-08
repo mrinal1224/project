851276c648ed3c194aa54e751cb6be3ce00c7b69
mrinal1224 <iammrinalkc@gmail.com>
2024-04-08 19:05:42 +0530

    push

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = 5003;
 const userRoutes = require('./routes/userRoute')
 
 mongoose
-  .connect("mongodb+srv://mrinalbhattacharya:9q4dH9oukh2Yk1pM@cluster0.ywjxjga.mongodb.net/BMS?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://mrinalbhattacharya:f3nBnwCYX9Sd9un1@cluster0.t4enktq.mongodb.net/BMS?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("DB connected"))
   .catch((err) => {
     console.log(err);
